207281fee6469fd81b103e0a5bfba70c21d02e2a
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2023-07-24 17:45:48 +0100

    Prisma, MySQL and PlanetScale setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,23 @@
+
+'use client';
+
+import { useStoreModal } from "@/hooks/useStoreModal";
+import { useEffect } from "react";
+
+export default function SetUpPage() {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen])
+
+    return (
+      <div className="p-4">
+        Root
+      </div>
+    )
+  }
+  
